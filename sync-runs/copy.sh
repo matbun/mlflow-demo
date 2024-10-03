1bd85217c8125b0b4f0ee1babbbcdd027d90e773
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-10-03 19:13:53 +0200

    Create copy.sh

---
+++ sync-runs/copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export MLFLOW_TRACKING_INSECURE_TLS='true'
+export MLFLOW_TRACKING_USERNAME='matteo.bunino@cern.ch'
+export MLFLOW_TRACKING_PASSWORD='YOUR_PWD'
+export MLFLOW_TRACKING_URI='https://mlflow.intertwin.fedcloud.eu/'
+
+copy-run --run-id $RUNID --experiment-name "MNIST_PyTorch_Experiment" --src-mlflow-uri mlruns --dst-mlflow-uri https://mlflow.intertwin.fedcloud.eu/
