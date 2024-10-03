e2b2cc69305bc89b81cb4824bdcabd84f7638967
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-10-03 19:20:36 +0200

    Update export.sh

--- sync-runs/export.sh
+++ sync-runs/export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Export a specific local run to a local folder called ./my-export using some intermediate "format".
+# Export a specific local run under ./mlruns to a local folder called ./my-export using some intermediate "format".
 # However, consider copying directly the run to the remote server using copy.sh instead of doing export.sh + import.sh.
 
 export MLFLOW_TRACKING_URI="file://${PWD}/mlruns"
